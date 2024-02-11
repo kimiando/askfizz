05fd73d6389f9e01286f59f92f3b1181d19367c9
kimiko ando <kimiko.ando13@icloud.com>
2024-02-11 11:29:03 +0900

    added a new migration for questions and created a model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_09_191306) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_11_022525) do
+  create_table "questions", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_09_191306) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "users"
 end

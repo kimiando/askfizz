1ef5cd93179f83328f3aff9fbd6b4d277a3c4596
kimiko ando <kimiko.ando13@icloud.com>
2024-02-15 05:44:01 +0900

    added a new migration for anonymous questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_12_232440) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_14_202453) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_12_232440) do
     t.datetime "updated_at", null: false
     t.integer "asked_to_id", null: false
     t.integer "asker_id", null: false
+    t.boolean "anonymous", default: false
     t.index ["asked_to_id"], name: "index_questions_on_asked_to_id"
     t.index ["asker_id"], name: "index_questions_on_asker_id"
     t.index ["user_id"], name: "index_questions_on_user_id"

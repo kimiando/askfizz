08116f30e5dbb6445d219e65a3785800eb68d43b
kimiko ando <kimiko.ando13@icloud.com>
2024-02-20 23:40:18 +0900

    search is not working yet, still needs to change the code

--- Gemfile
+++ Gemfile
@@ -68,6 +68,9 @@ gem "cloudinary"
 
 gem 'pg'
 
+# search
+gem "pg_search"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_18_102116) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_19_032010) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -47,6 +47,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_18_102116) do
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.integer "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "questions", force: :cascade do |t|
